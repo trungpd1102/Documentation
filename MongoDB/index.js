7c4048e16f07122dff06a37d1c0243f06f497153
trungpd1102 <51411834+trungpd1102@users.noreply.github.com>
2022-09-21 23:21:58 +0700

    Create index.js

---
+++ MongoDB/index.js
@@ -0,0 +1,6 @@
+If MongoDB is installed on macOS via Homebrew, the default data directory depends on the type of processor in the system.
+
+                   | Intel Processor	          |Apple M1 Processor
+Data Directory.    |/usr/local/var/mongodb	    |/opt/homebrew/var/mongodb
+Configuration file |/usr/local/etc/mongod.conf	|/opt/homebrew/etc/mongod.conf
+Log directory	     |/usr/local/var/log/mongodb	|/opt/homebrew/var/log/mongodb
